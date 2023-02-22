ccb9c619d0ed648a52ce71d03dbf70d500b8992a
Robin Rosner <robinrosner@outlook.de>
2023-02-22 09:49:50 +0100

    udp kinda

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -91,7 +91,7 @@ int main(void)
   MX_USART1_UART_Init();
   MX_LWIP_Init();
   /* USER CODE BEGIN 2 */
-
+  udpServer_init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -106,6 +106,66 @@ int main(void)
 		sys_check_timeouts();
 	}
   /* USER CODE END 3 */
+}
+void udp_receive_callback(void *arg, struct udp_pcb *upcb, struct pbuf *p, const ip_addr_t *addr, u16_t port)
+{
+	struct pbuf *txBuf;
+
+	/* Get the IP of the Client */
+	char *remoteIP = ipaddr_ntoa(addr);
+
+	char buf[100];
+
+
+	int len = sprintf (buf,"Hello %s From UDP SERVER\n", (char*)p->payload);
+
+	/* allocate pbuf from RAM*/
+	txBuf = pbuf_alloc(PBUF_TRANSPORT,len, PBUF_RAM);
+
+	/* copy the data into the buffer  */
+	pbuf_take(txBuf, buf, len);
+
+	/* Connect to the remote client */
+	udp_connect(upcb, addr, port);
+
+	/* Send a Reply to the Client */
+	udp_send(upcb, txBuf);
+
+	/* free the UDP connection, so we can accept new clients */
+	udp_disconnect(upcb);
+
+	/* Free the p_tx buffer */
+	pbuf_free(txBuf);
+
+	/* Free the p buffer */
+	pbuf_free(p);
+}
+
+void udpServer_init(void)
+{
+	// UDP Control Block structure
+   struct udp_pcb *upcb;
+   err_t err;
+
+   /* 1. Create a new UDP control block  */
+   upcb = udp_new();
+
+   /* 2. Bind the upcb to the local port */
+   ip_addr_t myIPADDR;
+   IP_ADDR4(&myIPADDR, 192, 168, 178, 222);
+
+   err = udp_bind(upcb, &myIPADDR, 7);  // 7 is the server UDP port
+
+
+   /* 3. Set a receive callback for the upcb */
+   if(err == ERR_OK)
+   {
+	   udp_recv(upcb, udp_receive_callback, NULL);
+   }
+   else
+   {
+	   udp_remove(upcb);
+   }
 }
 
 /**
